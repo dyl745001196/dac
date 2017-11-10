c8f4e57759aea28ded23c3c7c46346626edad65a
dyl745001196 <745001196@qq.com>
2017-11-10 15:12:57 +0800

    the data to do the si in in the mat file

--- high_level_call.m
+++ high_level_call.m
@@ -1,12 +1,26 @@
 close all 
 
-[dac_goal, dac_input] = dac_setup(1, [0:0.01:10], 1);
+%here we want to gather the data with different freq input like what we do
+%in the freqency analysis in si.
+dac_goal_whole_set = [];
+dac_input_whole_set = [];
 
-plot(dac_goal, 'b');
-hold on 
-plot(dac_input', 'r')
-legend('dac\_goal to do the si', 'dac\_input to do the si');
-grid on
-title('input with sin(1*t)')
-xlabel('time step')
-ylabel('value')
+for index = 1:10
+    init_value = 1;
+    freq = index;
+    [dac_goal, dac_input] = dac_setup(freq, [0:0.01:10], init_value);
+    subplot(3, 4, index)
+    plot(dac_goal, 'b');
+    hold on 
+    plot(dac_input', 'r')
+    %legend('dac\_goal to do the si', 'dac\_input to do the si');
+    grid on
+    title(['u=sin(' int2str(index) '*t)'])
+    xlabel('time step')
+    ylabel('value')
+    dac_goal_whole_set = [dac_goal_whole_set; dac_goal];
+    dac_input_whole_set = [dac_input_whole_set; dac_input];
+end
+
+
+save('si_dac_data', 'dac_goal_whole_set', 'dac_input_whole_set')
